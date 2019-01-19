21c650e61dc0f237ca43ed392fefe39be66d03be
terurin <hamusutaateru@live.jp>
2019-01-20 01:27:32 +0900

    add mattrix

--- controllor.h
+++ controllor.h
@@ -2,6 +2,7 @@
 #define __CONTROLLOR_HEADER_GUARD__
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 typedef int32_t q1516_t;
 typedef int64_t q3132_t;
@@ -31,13 +32,43 @@ typedef struct svec* svec_ptr;
 
 void svec_init(svec_ptr , q1516_t* array, size_t size);
 void svec_deinit(svec_ptr);
-q3132_t svec_dot(svec_ptr,svec_ptr);
+q3132_t svec_dot(const svec_ptr,const svec_ptr);
 svec_ptr svec_add(svec_ptr,const svec_ptr,const svec_ptr);
 svec_ptr svec_sub(svec_ptr,const svec_ptr,const svec_ptr);
 svec_ptr svec_neg(svec_ptr,const svec_ptr);
 svec_ptr svec_set(svec_ptr,q1516_t);
 q1516_t svec_idx(const svec_ptr,size_t);
-q1516_t svec_length2(svec_ptr,const svec_ptr);//|x|^2
-svec_ptr svec_normalize(svec_ptr,const svec_ptr);
+q1516_t svec_len2(const svec_ptr);//|x|^2
+q1516_t svec_len(const svec_ptr);//|x|
+svec_ptr svec_normalize(svec_ptr,const svec_ptr);//x/|x|
+svec_ptr svec_cmul(svec_ptr,const svec_ptr,q3132_t);
+
+struct smat{
+    size_t x,y;
+    q1516_t *mat;
+};
+
+typedef struct smat smat_t;
+typedef struct smat* smat_ptr;
+void smat_init(smat_ptr obj,q1516_t*mat,size_t x,size_t y);
+void smat_deinit(smat_ptr obj);
+bool smat_is_match(const smat_ptr,const smat_ptr);//大きさが等しいか
+bool smat_is_square(const smat_ptr);
+
+
+smat_ptr smat_set(smat_ptr,q1516_t);
+smat_ptr smat_unit(smat_ptr,q1516_t);
+q1516_t smat_idx(const smat_ptr,size_t x,size_t y);//安全
+smat_ptr smat_add(smat_ptr,const smat_ptr,const smat_ptr);
+smat_ptr smat_sub(smat_ptr,const smat_ptr,const smat_ptr);
+smat_ptr smat_cmul(smat_ptr,const smat_ptr,q1516_t);
+svec_ptr smat_vmul(svec_ptr y,const smat_ptr A,const svec_ptr x);//Ax'=y'
+smat_ptr smat_mul(smat_ptr d,const smat_ptr a,const smat_ptr b);//AB=D
+
+smat_ptr smat_cdiv(smat_ptr,const smat_ptr,q1516_t);
+smat_ptr smat_tri(smat_ptr,const smat_ptr);//三角行列を求める
+q1516_t smat_det(smat_ptr buf,const smat_ptr a);
+smat_ptr smat_inv2(smat_ptr,const smat_ptr);//2x2の逆行列
+//smat_ptr smat_inv(smat_ptr,const smat_ptr);//NxNの逆行列
 
 #endif
